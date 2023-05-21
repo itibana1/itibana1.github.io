249a62be3c1a19cb799593284dc87aee11b6e328
itibana1 <itibana1@gmail.com>
2023-05-20 20:51:41 -0500

    Primer version de Pagina Web
    Se ingresa el primer commit a la pagina web para el taller de la especializacion en Desarrollo de software

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("vscode-btn").addEventListener("click", function() {
+    window.location.href = "vscode.html";
+});
+
+document.getElementById("eclipse-btn").addEventListener("click", function() {
+    window.location.href = "eclipse.html";
+});
+
+document.getElementById("intellij-btn").addEventListener("click", function() {
+    window.location.href = "intellij.html";
+});
+
+document.getElementById("pycharm-btn").addEventListener("click", function() {
+    window.location.href = "pycharm.html";
+});
+
+document.getElementById("xcode-btn").addEventListener("click", function() {
+    window.location.href = "xcode.html";
+});
